29662c03227986091e08cda79d5583935c7afc8d
Sharozraw <rawsharoz2002@gmail.com>
2024-11-01 14:46:59 +0530

    Change

---
+++ 10-08 data stuct/10-08 data stuct/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_08_data_stuct
+{
+    public class DynamicArray
+    {
+        private int[] data;
+        private int capacity; //to define size
+        private int count;
+        public DynamicArray()
+        {
+            count = 0;
+            capacity = 5;
+
+            data = new int[capacity];
+        }
+        public void Print()
+        {
+            if(count == 0)
+            {
+                Console.WriteLine("DynamicArray is empty");
+                return;
+            }
+            for (int i= 0; i < count; i++)
+            {
+                Console.Write($"{data[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 10-08 data stuct/10-08 data stuct/Program.cs
@@ -0,0 +1,4 @@
+﻿using _10_08_data_stuct;
+DynamicArray array = new DynamicArray();
+
+array.Print();
